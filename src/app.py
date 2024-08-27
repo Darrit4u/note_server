86c56c5e9dd1493a0e2ea62d0e9921803372fc3f
Darrit <an.n.ag.75767@gmail.com>
2024-08-28 02:47:06 +0300

    add auth and regist, db based on json file and  request processing

--- src/app.py
+++ src/app.py
@@ -1,67 +1,101 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from json_db_lite import JSONDatabase
+from fastapi import FastAPI, Depends, HTTPException, Response, Request
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from typing import List, Dict, Optional
+from datetime import datetime, timedelta, timezone
 
-# инициализация объекта
-user_db = JSONDatabase(file_path='users.json')
-note_db = JSONDatabase(file_path='notes.json')
+from src.db import SUser, SNote
+from src.db import get_user_by_id, get_user_by_username, add_user, add_new_note, get_note_by_id_user, get_note_by_id_note
+from src.jwt_token import create_jwt_token, verify_jwt_token
 
 app = FastAPI()
 
-def get_users():
-    return user_db.get_all_records()
 
+# хэширование пароля
 
-def add_user(user: dict):
-    user_db.add_records(user)
-    return True
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
 
-def get_notes():
-    return note_db.get_all_records()
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 
+def get_token(request: Request):
+    token = request.cookies.get('users_access_token')
+    if not token:
+        raise HTTPException(status_code=401, detail='Token not found')
+    return token
 
-def add_note(data: dict):
-    note_db.add_records(data)
-    return True
+def get_current_user(token: str = Depends(get_token)) -> SUser:
+    payload = verify_jwt_token(token)
+    if payload is None:
+        raise HTTPException(status_code=401, detail='Token is not exist')
 
+    expire = payload.get('exp')
+    expire_time = datetime.fromtimestamp(int(expire), tz=timezone.utc)
+    if (not expire) or (expire_time < datetime.now(timezone.utc)):
+        raise HTTPException(status_code=401, detail='Токен истек')
 
-class User(BaseModel):
-    id: int
-    name: str
-    password: str
+    user_id = payload.get('sub')
+    if not user_id:
+        raise HTTPException(status_code=401, detail='Не найден ID пользователя')
 
+    user = get_user_by_id(int(user_id))
+    if not user:
+        raise HTTPException(status_code=401, detail='User not found')
 
-class Note(BaseModel):
-    id: int
-    id_user: int
-    text: str
+    return user
 
+# api
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 @app.post("/register")
-def register():
-    pass
+def register_user(username: str, password: str) -> Dict[str, bool]:
+    if get_user_by_username(username) is not None:
+        raise HTTPException(status_code=409, detail="User exists")
 
-@app.post("/auth")
-def auth():
-    pass
+    hashed_password = get_password_hash(password)
+    add_user(username, hashed_password)
+
+    return {"Success": True}
+
+@app.post("/login")
+def authenticate_user(response: Response, username: str, password: str):
+    user = get_user_by_username(username)
+    if user is None:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    is_password_correct = verify_password(password, user.password)
+
+    if not is_password_correct:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    jwt_token = create_jwt_token({"sub": user.id})
+    response.set_cookie(key="users_access_token", value=jwt_token, httponly=True)
+    return {"access_token": jwt_token, "token_type": "bearer"}
+
+@app.get("/user/note")
+def get_user_note(cur_user: SUser = Depends(get_current_user)) -> List[SNote]:
+    print(cur_user.id)
+    return get_note_by_id_user(int(cur_user.id))
 
 @app.get("/user/{id_user}")
-def get_user(id_user: int):
-    pass
-
-@app.get("/user/{id_user}/note")
-def get_note_by_user_id(id_user: int):
-    pass
-
-@app.get("/note/{id_note}")
-def get_note_by_id(id_note: int):
-    pass
+def get_user(id_user: int) -> SUser:
+    return get_user_by_id(id_user)
 
 @app.post("/note/add")
-def add_note(text: str):
-    pass
+def add_note(text: str, cur_user: SUser = Depends(get_current_user)) -> Dict[str, bool]:
+    id_user = cur_user.id
+    add_new_note(id_user=id_user, text=text)
+    return {"Success": True}
+
+@app.get("/note/{id_note}")
+def get_note_by_id(id_note: int, cur_user: SUser = Depends(get_current_user)) -> SNote:
+    note_data = get_note_by_id_note(id_note)
+    if cur_user.id != note_data.id_user:
+        raise HTTPException(status_code=400, detail='Incorrect user')
+    return note_data
---
+++ src/db.py
@@ -0,0 +1,111 @@
+from pydantic import BaseModel
+from fastapi import HTTPException
+from typing import List, Dict, Optional, Any
+import json
+
+user_file_path='users.json'
+note_file_path='notes.json'
+
+
+def read_from_file(file_path: str) -> Optional[List[Dict[str, Any]]]:
+    with open(file_path, 'r', encoding='utf-8') as f:
+        try:
+            return json.load(f)
+        except json.JSONDecodeError:
+            return []
+
+def find_records_by_key(file_path: str, key, value):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError:
+            return []
+        return [record for record in data if record.get(key) == value]
+
+def add_records(file_path: str, new_data: Dict[str, Any]):
+    data = read_from_file(file_path)
+    data.append(new_data)
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+
+class SUser(BaseModel):
+    id: int
+    username: str
+    password: str
+
+
+class SNote(BaseModel):
+    id: int
+    id_user: int
+    text: str
+
+
+def dict_to_SUser(user_data: dict) -> SUser:
+    return SUser(
+        id=user_data['id'],
+        username=user_data['username'],
+        password=user_data['password']
+    )
+
+def dict_to_SNote(note_data: dict) -> SNote:
+    return SNote(
+        id=note_data['id'],
+        id_user=note_data['id_user'],
+        text=note_data['text']
+    )
+
+
+def get_users() -> List[Dict[str, Any]]:
+    return read_from_file(user_file_path)
+
+def get_user_by_id(id_user: int) -> Optional[SUser]:
+    user_data = find_records_by_key(user_file_path, 'id', id_user)
+    if len(user_data) > 1:
+        raise HTTPException(status_code=400, detail="More than one user with this id!")
+    if len(user_data) == 0:
+        return None
+    return dict_to_SUser(user_data[0])
+
+def get_user_by_username(username: str) -> Optional[SUser]:
+    user_data = find_records_by_key(user_file_path, 'username', username)
+    if len(user_data) > 1:
+        raise HTTPException(status_code=400, detail="More than one user with this username!")
+    if len(user_data) == 0:
+        return None
+    return dict_to_SUser(user_data[0])
+
+def get_last_id_user() -> int:
+    data = read_from_file(user_file_path)
+    if len(data) > 0:
+        return data[-1]['id']
+    return 0
+
+def add_user(name: str, password: str) -> bool:
+    id_user = get_last_id_user() + 1
+    user = SUser(id=id_user, username=name, password=password)
+    add_records(user_file_path, user.model_dump())
+    return True
+
+def get_notes():
+    return read_from_file(note_file_path)
+
+def get_note_by_id_note(id_note: int) -> SNote:
+    note_data = find_records_by_key(note_file_path,'id', id_note)
+    return dict_to_SNote(note_data[0])
+
+def get_note_by_id_user(id_user: int) -> List[SNote]:
+    notes_data = find_records_by_key(note_file_path, 'id_user', id_user)
+    return [dict_to_SNote(x) for x in notes_data]
+
+def get_last_note_id() -> int:
+    data = read_from_file(note_file_path)
+    if len(data) > 0:
+        return int(data[-1]['id'])
+    return 0
+
+def add_new_note(id_user: int, text: str) -> bool:
+    id_note = get_last_note_id()+1
+    data = SNote(id=id_note, id_user=id_user, text=text)
+    add_records(note_file_path, data.model_dump())
+    return True
