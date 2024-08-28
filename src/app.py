8dce9f67218777927ad32d99f3a904f7b32e906e
Darrit <an.n.ag.75767@gmail.com>
2024-08-28 19:23:16 +0300

    make db class

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Any
 from datetime import datetime, timezone
 from fastapi import FastAPI, Depends, HTTPException, Response, Request
 from fastapi.security import OAuth2PasswordBearer
@@ -6,17 +6,16 @@ from passlib.context import CryptContext
 import requests
 
 from src.db import SUser, SNote
-from src.db import (
-    get_user_by_id,
-    get_user_by_username,
-    add_user,
-    add_new_note,
-    get_note_by_id_user,
-    get_note_by_id_note,
-)
+from src.db import JsonDB
 from src.jwt_token import create_jwt_token, verify_jwt_token
 
 app = FastAPI()
+
+session_db = JsonDB(user_file_path="users.json", note_file_path="notes.json")
+
+
+def get_db() -> JsonDB:
+    return session_db
 
 
 # хэширование пароля
@@ -33,14 +32,16 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_token(request: Request):
+def get_token(request: Request) -> str:
     token = request.cookies.get("users_access_token")
     if not token:
         raise HTTPException(status_code=401, detail="Token not found")
     return token
 
 
-def get_current_user(token: str = Depends(get_token)) -> SUser:
+def get_current_user(
+    token: str = Depends(get_token), db: JsonDB = Depends(get_db)
+) -> SUser:
     payload = verify_jwt_token(token)
     if payload is None:
         raise HTTPException(status_code=401, detail="Token is not exist")
@@ -54,7 +55,7 @@ def get_current_user(token: str = Depends(get_token)) -> SUser:
     if not user_id:
         raise HTTPException(status_code=401, detail="Не найден ID пользователя")
 
-    user = get_user_by_id(int(user_id))
+    user = db.get_user_by_id(int(user_id))
     if not user:
         raise HTTPException(status_code=401, detail="User not found")
 
@@ -65,24 +66,28 @@ def get_current_user(token: str = Depends(get_token)) -> SUser:
 
 
 @app.get("/")
-def root():
+def root() -> Dict[str, str]:
     return {"message": "Hello World"}
 
 
 @app.post("/register")
-def register_user(username: str, password: str) -> Dict[str, bool]:
-    if get_user_by_username(username) is not None:
+def register_user(
+    username: str, password: str, db: JsonDB = Depends(get_db)
+) -> Dict[str, bool]:
+    if db.get_user_by_username(username) is not None:
         raise HTTPException(status_code=409, detail="User exists")
 
     hashed_password = get_password_hash(password)
-    add_user(username, hashed_password)
+    db.add_user(username, hashed_password)
 
     return {"Success": True}
 
 
 @app.post("/login")
-def authenticate_user(response: Response, username: str, password: str):
-    user = get_user_by_username(username)
+def authenticate_user(
+    response: Response, username: str, password: str, db: JsonDB = Depends(get_db)
+) -> Dict[str, Any]:
+    user = db.get_user_by_username(username)
     if user is None:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
@@ -96,33 +101,44 @@ def authenticate_user(response: Response, username: str, password: str):
 
 
 @app.get("/user/note")
-def get_user_note(cur_user: SUser = Depends(get_current_user)) -> List[SNote]:
-    return get_note_by_id_user(int(cur_user.id))
+def get_user_note(
+    cur_user: SUser = Depends(get_current_user), db: JsonDB = Depends(get_db)
+) -> List[SNote]:
+    return db.get_note_by_id_user(int(cur_user.id))
 
 
 @app.get("/user/{id_user}")
-def get_user(id_user: int) -> SUser:
-    return get_user_by_id(id_user)
+def get_user(id_user: int, db: JsonDB = Depends(get_db)) -> SUser:
+    return db.get_user_by_id(id_user)
 
 
 @app.post("/note/add")
-def add_note(text: str, cur_user: SUser = Depends(get_current_user)) -> Dict[str, bool]:
+def add_note(
+    text: str, cur_user: SUser = Depends(get_current_user), db: JsonDB = Depends(get_db)
+) -> Dict[str, bool]:
     response = requests.get(
         "https://speller.yandex.net/services/spellservice.json/checkText",
         params={"text": text},
+        timeout=10,
     )
 
     if response.json():
         raise HTTPException(status_code=418, detail=response.json())
 
     id_user = cur_user.id
-    add_new_note(id_user=id_user, text=text)
+    db.add_new_note(id_user=id_user, text=text)
     return {"Success": True}
 
 
 @app.get("/note/{id_note}")
-def get_note_by_id(id_note: int, cur_user: SUser = Depends(get_current_user)) -> SNote:
-    note_data = get_note_by_id_note(id_note)
+def get_note_by_id(
+    id_note: int,
+    cur_user: SUser = Depends(get_current_user),
+    db: JsonDB = Depends(get_db),
+) -> SNote:
+    note_data = db.get_note_by_id_note(id_note)
     if cur_user.id != note_data.id_user:
-        raise HTTPException(status_code=401, detail="You don't have access to this note")
+        raise HTTPException(
+            status_code=401, detail="You don't have access to this note"
+        )
     return note_data
--- src/db.py
+++ src/db.py
@@ -4,34 +4,6 @@ from pydantic import BaseModel
 from fastapi import HTTPException
 
 
-USER_FILE_PATH = "users.json"
-NOTE_FILE_PATH = "notes.json"
-
-
-def read_from_file(file_path: str) -> Optional[List[Dict[str, Any]]]:
-    with open(file_path, "r", encoding="utf-8") as f:
-        try:
-            return json.load(f)
-        except json.JSONDecodeError:
-            return []
-
-
-def find_records_by_key(file_path: str, key, value):
-    with open(file_path, "r", encoding="utf-8") as f:
-        try:
-            data = json.load(f)
-        except json.JSONDecodeError:
-            return []
-        return [record for record in data if record.get(key) == value]
-
-
-def add_records(file_path: str, new_data: Dict[str, Any]):
-    data = read_from_file(file_path)
-    data.append(new_data)
-    with open(file_path, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4)
-
-
 class SUser(BaseModel):
     id: int
     username: str
@@ -44,81 +16,102 @@ class SNote(BaseModel):
     text: str
 
 
-def dict_to_SUser(user_data: dict) -> SUser:
-    return SUser(
-        id=user_data["id"],
-        username=user_data["username"],
-        password=user_data["password"],
-    )
+class JsonDB:
+    def __init__(self, user_file_path: str, note_file_path: str):
+        self.user_file_path = user_file_path
+        self.note_file_path = note_file_path
 
+    @staticmethod
+    def read_from_file(file_path: str) -> Optional[List[Dict[str, Any]]]:
+        with open(file_path, "r", encoding="utf-8") as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return []
 
-def dict_to_SNote(note_data: dict) -> SNote:
-    return SNote(
-        id=note_data["id"], id_user=note_data["id_user"], text=note_data["text"]
-    )
+    @staticmethod
+    def find_records_by_key(file_path: str, key, value):
+        with open(file_path, "r", encoding="utf-8") as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                return []
+            return [record for record in data if record.get(key) == value]
 
+    def add_records(self, file_path: str, new_data: Dict[str, Any]):
+        data = self.read_from_file(file_path)
+        data.append(new_data)
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
 
-def get_users() -> List[Dict[str, Any]]:
-    return read_from_file(USER_FILE_PATH)
-
-
-def get_user_by_id(id_user: int) -> Optional[SUser]:
-    user_data = find_records_by_key(USER_FILE_PATH, "id", id_user)
-    if len(user_data) > 1:
-        raise HTTPException(status_code=400, detail="More than one user with this id!")
-    if len(user_data) == 0:
-        return None
-    return dict_to_SUser(user_data[0])
-
-
-def get_user_by_username(username: str) -> Optional[SUser]:
-    user_data = find_records_by_key(USER_FILE_PATH, "username", username)
-    if len(user_data) > 1:
-        raise HTTPException(
-            status_code=400, detail="More than one user with this username!"
+    @staticmethod
+    def dict_to_SUser(user_data: dict) -> SUser:
+        return SUser(
+            id=user_data["id"],
+            username=user_data["username"],
+            password=user_data["password"],
         )
-    if len(user_data) == 0:
-        return None
-    return dict_to_SUser(user_data[0])
 
+    @staticmethod
+    def dict_to_SNote(note_data: dict) -> SNote:
+        return SNote(
+            id=note_data["id"], id_user=note_data["id_user"], text=note_data["text"]
+        )
 
-def get_last_id_user() -> int:
-    data = read_from_file(USER_FILE_PATH)
-    if len(data) > 0:
-        return data[-1]["id"]
-    return 0
+    def get_users(self) -> Optional[List[Dict[str, Any]]]:
+        return self.read_from_file(self.user_file_path)
 
+    def get_user_by_id(self, id_user: int) -> Optional[SUser]:
+        user_data = self.find_records_by_key(self.user_file_path, "id", id_user)
+        if len(user_data) > 1:
+            raise HTTPException(
+                status_code=400, detail="More than one user with this id!"
+            )
+        if len(user_data) == 0:
+            return None
+        return self.dict_to_SUser(user_data[0])
 
-def add_user(name: str, password: str) -> bool:
-    id_user = get_last_id_user() + 1
-    user = SUser(id=id_user, username=name, password=password)
-    add_records(USER_FILE_PATH, user.model_dump())
-    return True
+    def get_user_by_username(self, username: str) -> Optional[SUser]:
+        user_data = self.find_records_by_key(self.user_file_path, "username", username)
+        if len(user_data) > 1:
+            raise HTTPException(
+                status_code=400, detail="More than one user with this username!"
+            )
+        if len(user_data) == 0:
+            return None
+        return self.dict_to_SUser(user_data[0])
 
+    def get_last_id_user(self) -> int:
+        data = self.read_from_file(self.user_file_path)
+        if len(data) > 0:
+            return data[-1]["id"]
+        return 0
 
-def get_notes():
-    return read_from_file(NOTE_FILE_PATH)
+    def add_user(self, name: str, password: str) -> bool:
+        id_user = self.get_last_id_user() + 1
+        user = SUser(id=id_user, username=name, password=password)
+        self.add_records(self.user_file_path, user.model_dump())
+        return True
 
+    def get_notes(self) -> Optional[List[Dict[str, Any]]]:
+        return self.read_from_file(self.note_file_path)
 
-def get_note_by_id_note(id_note: int) -> SNote:
-    note_data = find_records_by_key(NOTE_FILE_PATH, "id", id_note)
-    return dict_to_SNote(note_data[0])
+    def get_note_by_id_note(self, id_note: int) -> SNote:
+        note_data = self.find_records_by_key(self.note_file_path, "id", id_note)
+        return self.dict_to_SNote(note_data[0])
 
+    def get_note_by_id_user(self, id_user: int) -> List[SNote]:
+        notes_data = self.find_records_by_key(self.note_file_path, "id_user", id_user)
+        return [self.dict_to_SNote(x) for x in notes_data]
 
-def get_note_by_id_user(id_user: int) -> List[SNote]:
-    notes_data = find_records_by_key(NOTE_FILE_PATH, "id_user", id_user)
-    return [dict_to_SNote(x) for x in notes_data]
+    def get_last_note_id(self) -> int:
+        data = self.read_from_file(self.note_file_path)
+        if len(data) > 0:
+            return int(data[-1]["id"])
+        return 0
 
-
-def get_last_note_id() -> int:
-    data = read_from_file(NOTE_FILE_PATH)
-    if len(data) > 0:
-        return int(data[-1]["id"])
-    return 0
-
-
-def add_new_note(id_user: int, text: str) -> bool:
-    id_note = get_last_note_id() + 1
-    data = SNote(id=id_note, id_user=id_user, text=text)
-    add_records(NOTE_FILE_PATH, data.model_dump())
-    return True
+    def add_new_note(self, id_user: int, text: str) -> bool:
+        id_note = self.get_last_note_id() + 1
+        data = SNote(id=id_note, id_user=id_user, text=text)
+        self.add_records(self.note_file_path, data.model_dump())
+        return True
